c14c1a42020a30e5aa120d412b84562b44ed10f8
Vitushan <vitushan@outlook.fr>
2025-06-14 19:46:57 +0200

    task Review

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -2,7 +2,8 @@
 
 
 from models.base_model import BaseModel
-
+from models.user import User
+from models.place import Place
 
 class Review(BaseModel):
     def __init__(self, place, user, comment, rating):
@@ -11,3 +12,18 @@ class Review(BaseModel):
         self.user = user
         self.comment = comment
         self.rating = rating
+
+        if not isinstance(comment, str):
+            raise TypeError("Reviews contain is mandatory")
+        
+        if not isinstance(rating, int):
+            raise TypeError("Rating must be a integer")
+        if rating < 1 or rating > 5:
+            raise ValueError("Rating between 1 and 5")
+
+
+        if not isinstance(user, User):
+            raise TypeError("User is a user instance")
+
+        if not isinstance(place, Place):
+            raise TypeError("Place is a place instance")
