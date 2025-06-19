8d1ecfe864dba713d7ed43bf77b019323a50c3f0
Vitushan <vitushan@outlook.fr>
2025-06-19 15:25:54 +0200

    feat: added files tests

---
+++ part2/tests/test_amenity.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is a module for interpreting python3
+"""
+
+
+from app.models.amenity import Amenity
+
+def test_amenity_creation():
+    """
+    This is a test for create amenity with name and description.
+    """
+    amenity = Amenity(name="Wi-Fi", description="This is a description")
+    assert amenity.name == "Wi-Fi"
+    assert amenity.description == "This is a description"
+    print("Amenity creation test passed!")
+
+test_amenity_creation()
