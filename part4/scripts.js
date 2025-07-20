580d83bc5e93fe902d1e9a31693716e27726edf7
Vitushan <vitushan@outlook.fr>
2025-07-20 13:58:20 +0200

    verify form with Checkvalidity and preventDefault

--- part4/scripts.js
+++ part4/scripts.js
@@ -6,16 +6,25 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     /* DO SOMETHING */
-    const login = document.getElementById('login-form');
-    if (login) {
-      login.addEventListener('submit', async (event) => {
+    const loginForm = document.getElementById('login-form');
+    if (loginForm) {
+      loginForm.addEventListener('submit', async (event) => {
         event.preventDefault();
 
-      if (!login.checkValidity()) {
-        login.reportValidity()
+      if (!loginForm.checkValidity()) {
+        loginForm.reportValidity()
         return;
       }
-
+      async function loginUser(email, password) {
+      const response = await fetch('https://your-api-url/login', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ email, password })
+      });
+      // Handle the response
+  }
       });
     }
   });
