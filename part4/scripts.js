9c4b8d4522b14a9092f9e3d8918e675c41af43d6
Vitushan <vitushan@outlook.fr>
2025-07-20 13:51:01 +0200

    verify form with Checkvalidity and preventDefault

--- part4/scripts.js
+++ part4/scripts.js
@@ -3,6 +3,19 @@
   Please, follow the project instructions to complete the tasks.
 */
 
+
 document.addEventListener('DOMContentLoaded', () => {
     /* DO SOMETHING */
+    const login = document.getElementById('login-form');
+    if (login) {
+      login.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+      if (!login.checkValidity()) {
+        login.reportValidity()
+        return;
+      }
+
+      });
+    }
   });
